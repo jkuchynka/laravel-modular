8eed6d92402348464ef8cad674189dcc9d9a0516
Jason Kuchynka <jason.kuchynka@gmail.com>
2020-10-18 00:00:22 -0700

    Allow groups and child routes in module routes config

--- src/Concerns/LoadsRoutes.php
+++ src/Concerns/LoadsRoutes.php
@@ -11,43 +11,49 @@ trait LoadsRoutes
 
     public function loadRoutes()
     {
-        Route::group([
+        // First level of routes should be array of groups
+        foreach ($this->get('routes', []) as $group) {
+            Route::group([
+                'prefix' => $group['prefix'],
+                'middleware' => $group['middleware']
+            ], function () use ($group) {
+                foreach ($group['routes'] as $moduleRoute) {
+                    $builder = new RouteBuilder($this, $moduleRoute);
 
-        ], function () {
-            foreach ($this->get('routes', []) as $moduleRoute) {
-                $builder = new RouteBuilder($this, $moduleRoute);
-
-                if ($builder->isResource()) {
-                    $name = $builder->getName();
-                    if ($builder->getMethod() === 'api-resource') {
-                        Route::apiResource($builder->getUri(), $builder->getUses(), [
-                           'as' => $name
-                        ])->names([
-                            'index' => $name.'.index',
-                            'store' => $name.'.store',
-                            'show' => $name.'.show',
-                            'update' => $name.'.update',
-                            'destroy' => $name.'.destroy'
-                        ]);
+                    if ($builder->isResource()) {
+                        $name = $builder->getName();
+                        if ($builder->getMethod() === 'api-resource') {
+                            Route::apiResource($builder->getUri(), $builder->getUses(), [
+                                'as' => $name
+                            ])->names([
+                                'index' => $name . '.index',
+                                'store' => $name . '.store',
+                                'show' => $name . '.show',
+                                'update' => $name . '.update',
+                                'destroy' => $name . '.destroy'
+                            ]);
+                        } else {
+                            Route::resource($builder->getUri(), $builder->getUses(), [
+                                'as' => $name
+                            ])->names([
+                                'index' => $name . '.index',
+                                'create' => $name . '.create',
+                                'store' => $name . '.store',
+                                'show' => $name . '.show',
+                                'edit' => $name . '.edit',
+                                'update' => $name . '.update',
+                                'destroy' => $name . '.destroy'
+                            ]);
+                        }
                     } else {
-                        Route::resource($builder->getUri(), $builder->getUses(), [
-                            'as' => $name
-                        ])->names([
-                            'index' => $name.'.index',
-                            'store' => $name.'.store',
-                            'show' => $name.'.show',
-                            'update' => $name.'.update',
-                            'destroy' => $name.'.destroy'
+                        $method = $builder->getMethod();
+                        Route::$method($builder->getUri(), [
+                            'as' => $builder->getName(),
+                            'uses' => $builder->getUses()
                         ]);
                     }
-                } else {
-                    $method = $builder->getMethod();
-                    Route::$method($builder->getUri(), [
-                        'as' => $builder->getName(),
-                        'uses' => $builder->getUses()
-                    ]);
                 }
-            }
-        });
+            });
+        }
     }
 }
